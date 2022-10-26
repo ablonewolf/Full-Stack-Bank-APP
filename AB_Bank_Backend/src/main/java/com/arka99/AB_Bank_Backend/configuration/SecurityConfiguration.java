761d9cc5398d57357aae0e6ab659d1e54d257163
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2022-10-27 02:54:21 +0600

    Implemented inmemoryuserdetails manager using default password encoder

--- AB_Bank_Backend/src/main/java/com/arka99/AB_Bank_Backend/configuration/SecurityConfiguration.java
+++ AB_Bank_Backend/src/main/java/com/arka99/AB_Bank_Backend/configuration/SecurityConfiguration.java
@@ -3,6 +3,9 @@ package com.arka99.AB_Bank_Backend.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +20,30 @@ public class SecurityConfiguration {
                 .and().httpBasic();
         return httpSecurity.build();
 
+//        denying all requests to any services
+
+//        httpSecurity.authorizeRequests().anyRequest().denyAll().and().formLogin().and().httpBasic();
+//        return httpSecurity.build();
+
+//        allowing any request to all microservices without any authorization
+//        httpSecurity.authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
+//        return httpSecurity.build();
+
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+//        using the inmemory userdetailsmanager with default password encoder
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("user")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);
     }
 }
