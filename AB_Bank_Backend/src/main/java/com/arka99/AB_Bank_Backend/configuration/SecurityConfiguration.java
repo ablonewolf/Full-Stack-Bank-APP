69873e44c94ab07a8e5c46b83bf519bdf2ca1e9f
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2022-10-27 16:18:42 +0600

    Implemented JDBCUserDetailsManager using NoOpPasswordEncoder with a demo entry on the datasource

--- AB_Bank_Backend/src/main/java/com/arka99/AB_Bank_Backend/configuration/SecurityConfiguration.java
+++ AB_Bank_Backend/src/main/java/com/arka99/AB_Bank_Backend/configuration/SecurityConfiguration.java
@@ -6,10 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
@@ -34,31 +38,36 @@ public class SecurityConfiguration {
 
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-//        using the InMemoryUserDetailsManager with default password encoder
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+////        using the InMemoryUserDetailsManager with default password encoder
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("12345")
+////                .authorities("admin")
+////                .build();
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("12345")
+////                .authorities("user")
+////                .build();
+////        return new InMemoryUserDetailsManager(admin,user);
+//
+////        Using the InMemoryUserDetailsManager with a separate Bean of PasswordEncoder
+//        UserDetails admin = User.withUsername("admin")
 //                .password("12345")
 //                .authorities("admin")
 //                .build();
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
+//        UserDetails user = User.withUsername("user")
 //                .password("12345")
 //                .authorities("user")
 //                .build();
 //        return new InMemoryUserDetailsManager(admin,user);
+//    }
 
-//        Using the InMemoryUserDetailsManager with a separate Bean of PasswordEncoder
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("user")
-                .build();
-        return new InMemoryUserDetailsManager(admin,user);
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
 //    Using a bean of NoOpsPasswordEncoder
