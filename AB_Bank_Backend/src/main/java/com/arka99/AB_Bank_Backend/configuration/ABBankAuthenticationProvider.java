a670975030f80a1e19c8ab920a6de6bcce9fbe46
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2022-11-02 06:42:47 +0600

    Implemented custom authentication provider and removed the custom user details service since it's redundant.

---
+++ AB_Bank_Backend/src/main/java/com/arka99/AB_Bank_Backend/configuration/ABBankAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package com.arka99.AB_Bank_Backend.configuration;
+
+import com.arka99.AB_Bank_Backend.model.Customer;
+import com.arka99.AB_Bank_Backend.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ABBankAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        List<Customer> customers = customerRepository.findCustomerByEmail(username);
+        if(customers.size()>0) {
+            if(passwordEncoder.matches(password,customers.get(0).getPassword())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username,password,authorities);
+            }
+            else {
+                throw new BadCredentialsException("Invalid password");
+            }
+        }
+        else {
+            throw new BadCredentialsException("No user exists with this username");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
--- AB_Bank_Backend/src/main/resources/sqlscripts/abbank.sql
+++ AB_Bank_Backend/src/main/resources/sqlscripts/abbank.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 27, 2022 at 01:33 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Nov 02, 2022 at 01:41 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -58,8 +59,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `email`, `password`, `role`) VALUES
-(1, 'arka.bhuiyan@bjitgroup.com', '12345', 'admin'),
-(2, 'nipa.howlader@bjitgroup.com', '12345', 'user');
+(1, 'arka.bhuiyan@bjitgroup.com', '$2a$12$sHU0KQxZINz9Y1JnvMg.l.rnjnPqxHslbgCujbsDooZxA3nRxFbCO', 'admin'),
+(2, 'nipa.howlader@bjitgroup.com', '$2a$12$sHU0KQxZINz9Y1JnvMg.l.rnjnPqxHslbgCujbsDooZxA3nRxFbCO', 'user');
 
 -- --------------------------------------------------------
 
@@ -134,7 +135,7 @@ ALTER TABLE `authorities`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
