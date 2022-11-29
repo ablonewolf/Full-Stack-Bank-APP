7b2b9279391df3df8f624e1b9e3aa1c946d30725
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2022-11-29 15:42:36 +0600

    Updated the database.

--- AB_Bank_Backend/src/main/resources/sqlscripts/abbank.sql
+++ AB_Bank_Backend/src/main/resources/sqlscripts/abbank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 11, 2022 at 07:13 AM
+-- Generation Time: Nov 29, 2022 at 10:41 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
